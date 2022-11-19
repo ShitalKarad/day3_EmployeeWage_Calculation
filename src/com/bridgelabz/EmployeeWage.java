e23c768e25a56740f64e2159653f8c540a833dad
ShitalKarad <shitalkarad24@gmail.com>
2022-11-19 17:59:44 +0530

    Added: Epmployee total working hours or days is reached for a month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,10 +6,11 @@ public class EmployeeWage {
     static final int PART_TIME_HOUR = 4;
     static final int IS_FULL_TIME = 2;
     static final int IS_PART_TIME = 1;
+    static final int TOTAL_WORKING_HOURS = 100;
     static final int WORKING_DAYS = 20;
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage");
-        int employeeCheck = (int) Math.floor(Math.random()*10)%4;
+        int employeeCheck = (int) Math.floor(Math.random()*10)%5;
         System.out.println(employeeCheck);
         int dailyWage = 0;
         if(employeeCheck == 1) {
@@ -22,12 +23,16 @@ public class EmployeeWage {
             dailyWage =FULL_DAY_HOUR * WAGE_PER_HOUR * WORKING_DAYS;
             System.out.println("Employee wage for a month is ");
         }
+        else if (employeeCheck == 4) {
+            dailyWage =TOTAL_WORKING_HOURS * WAGE_PER_HOUR * WORKING_DAYS;
+            System.out.println("Employee is present for all month   ");
+        }
 
 
         else {
 
                 System.out.println("Employee is Absent");
         }
-        System.out.println("Employee daily wage is " + dailyWage);
+        System.out.println("Employee wage is " + dailyWage);
     }
 }
